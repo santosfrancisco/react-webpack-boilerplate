22606ab39ecbac898c62451d17af448cfb2ef4cb
Francisco Santos <yoshi.df@gmail.com>
2018-08-12 21:53:34 -0300

    chore: creates entrypoint file
    creates a index.js and index.html template for webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import App from './app'
+const App = () => <h1>Hello</h1>
+
+ReactDOM.render(<App />, document.getElementById('root'))
